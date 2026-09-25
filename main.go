5d0ad5dd5660e52a37cc8c91fbc874cbebf665f3
ivansizov <sizovivan.05@gmail.com>
2026-09-26 01:51:52 +0700

    selected-preselected squares count

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log/slog"
 	"slices"
+	"strconv"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -16,52 +17,142 @@ const (
 	squareToggleCooldown = 0.05
 )
 
+type Square struct {
+	Pos      rl.Vector2
+	Selected bool
+}
+
 type Game struct {
-	squares      []rl.Vector2
-	nextToggleAt float64
+	squares        []Square
+	nextToggleAt   float64
+	selectionStart rl.Vector2
+	selecting      bool
 }
 
-func (g *Game) Init() {
+func main() {
+	var game Game
+	game.run()
+}
+
+func (g *Game) run() {
 	rl.InitWindow(windowWidth, windowHeight, windowTitle)
+	defer rl.CloseWindow()
 	rl.SetTargetFPS(targetFPS)
+
+	for !rl.WindowShouldClose() {
+		g.update()
+
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+		g.draw()
+		rl.EndDrawing()
+	}
 }
 
-func (g *Game) ToggleSquare(position rl.Vector2) {
-	if index := slices.Index(g.squares, position); index >= 0 {
-		g.squares = slices.Delete(g.squares, index, index+1)
-		slog.Info("square removed", "x", position.X, "y", position.Y)
-		return
+func (g *Game) update() {
+	if rl.IsKeyDown(rl.KeyA) {
+		g.toggleSquare(rl.GetMousePosition(), rl.GetTime())
 	}
 
-	g.squares = append(g.squares, position)
-	slog.Info("square created", "x", position.X, "y", position.Y)
+	if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
+		g.selecting = true
+		g.selectionStart = rl.GetMousePosition()
+	}
+
+	if rl.IsMouseButtonReleased(rl.MouseButtonLeft) && g.selecting {
+		g.selectSquares(selectionRect(g.selectionStart, rl.GetMousePosition()))
+		g.selecting = false
+	}
 }
 
-func (g *Game) TryToggleSquare(position rl.Vector2, now float64) bool {
+func (g *Game) draw() {
+	var selection rl.Rectangle
+	if g.selecting {
+		selection = selectionRect(g.selectionStart, rl.GetMousePosition())
+		rl.DrawRectangleRec(selection, rl.Fade(rl.SkyBlue, 0.2))
+	}
+
+	selected := 0
+	preSelected := 0
+	for _, square := range g.squares {
+		rect := squareRect(square.Pos)
+		color := rl.RayWhite
+		if square.Selected {
+			color = rl.Gold
+			selected++
+		}
+		if g.selecting && rl.CheckCollisionRecs(rect, selection) {
+			color = rl.Orange
+			preSelected++
+		}
+
+		rl.DrawRectangleRec(rect, color)
+	}
+
+	if g.selecting {
+		rl.DrawRectangleLinesEx(selection, 1, rl.SkyBlue)
+	}
+
+	rl.DrawText("Selected: "+strconv.Itoa(selected), 10, 10, 20, rl.Green)
+	rl.DrawText("PreSelected: "+strconv.Itoa(preSelected), 10, 30, 20, rl.Gray)
+}
+
+func (g *Game) toggleSquare(position rl.Vector2, now float64) bool {
 	if now < g.nextToggleAt {
 		return false
 	}
 
-	g.ToggleSquare(position)
+	if index := slices.IndexFunc(g.squares, func(square Square) bool {
+		return square.Pos == position
+	}); index >= 0 {
+		g.squares = slices.Delete(g.squares, index, index+1)
+		slog.Info("square removed", "x", position.X, "y", position.Y)
+	} else {
+		g.squares = append(g.squares, Square{Pos: position})
+		slog.Info("square created", "x", position.X, "y", position.Y)
+	}
+
 	g.nextToggleAt = now + squareToggleCooldown
 	return true
 }
 
-func main() {
-	g := &Game{}
-	g.Init()
-	defer rl.CloseWindow()
+func (g *Game) selectSquares(rect rl.Rectangle) {
+	for i := range g.squares {
+		g.squares[i].Selected = rl.CheckCollisionRecs(squareRect(g.squares[i].Pos), rect)
+	}
+}
 
-	for !rl.WindowShouldClose() {
-		if rl.IsKeyDown(rl.KeyA) {
-			g.TryToggleSquare(rl.GetMousePosition(), rl.GetTime())
-		}
+func squareRect(position rl.Vector2) rl.Rectangle {
+	return rl.Rectangle{
+		X:      position.X - squareSize/2,
+		Y:      position.Y - squareSize/2,
+		Width:  squareSize,
+		Height: squareSize,
+	}
+}
 
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.Black)
-		for _, square := range g.squares {
-			rl.DrawRectangle(int32(square.X)-squareSize/2, int32(square.Y)-squareSize/2, squareSize, squareSize, rl.RayWhite)
+func selectionRect(start, end rl.Vector2) rl.Rectangle {
+	if start == end {
+		return rl.Rectangle{
+			X:      start.X,
+			Y:      start.Y,
+			Width:  1,
+			Height: 1,
 		}
-		rl.EndDrawing()
+	}
+
+	if start.X > end.X {
+		start.X, end.X = end.X, start.X
+	}
+
+	if start.Y > end.Y {
+		start.Y, end.Y = end.Y, start.Y
+	}
+
+	return rl.Rectangle{
+		X:      start.X,
+		Y:      start.Y,
+		Width:  end.X - start.X,
+		Height: end.Y - start.Y,
 	}
 }
--- main_test.go
+++ main_test.go
@@ -10,14 +10,36 @@ func TestToggleSquare(t *testing.T) {
 	g := &Game{}
 	position := rl.Vector2{X: 12, Y: 34}
 
-	g.ToggleSquare(position)
-	if len(g.squares) != 1 || g.squares[0] != position {
+	g.toggleSquare(position, 1)
+	if len(g.squares) != 1 || g.squares[0].Pos != position {
 		t.Fatalf("squares = %#v, want [%#v]", g.squares, position)
 	}
 
-	g.ToggleSquare(position)
+	g.toggleSquare(position, 1+squareToggleCooldown)
 	if len(g.squares) != 0 {
 		t.Fatalf("squares = %#v, want empty", g.squares)
+	}
+}
+
+func TestSelectSquares(t *testing.T) {
+	g := &Game{squares: []Square{
+		{Pos: rl.Vector2{X: 25, Y: 10}},
+		{Pos: rl.Vector2{X: 50, Y: 50}, Selected: true},
+	}}
+
+	g.selectSquares(rl.Rectangle{X: 0, Y: 0, Width: 20, Height: 20})
+
+	if !g.squares[0].Selected || g.squares[1].Selected {
+		t.Fatalf("selected = %#v, want [true false]", g.squares)
+	}
+}
+
+func TestSelectionRectForClickUsesCursorPoint(t *testing.T) {
+	position := rl.Vector2{X: 12, Y: 34}
+	rect := selectionRect(position, position)
+
+	if rect.X != position.X || rect.Y != position.Y || rect.Width != 1 || rect.Height != 1 {
+		t.Fatalf("selectionRect() = %#v, want a 1x1 rectangle at %#v", rect, position)
 	}
 }
 
@@ -25,13 +47,13 @@ func TestTryToggleSquareRespectsCooldown(t *testing.T) {
 	g := &Game{}
 	position := rl.Vector2{X: 12, Y: 34}
 
-	if !g.TryToggleSquare(position, 1) {
+	if !g.toggleSquare(position, 1) {
 		t.Fatal("first toggle was ignored")
 	}
-	if g.TryToggleSquare(position, 1+squareToggleCooldown/2) {
+	if g.toggleSquare(position, 1+squareToggleCooldown/2) {
 		t.Fatal("toggle during cooldown succeeded")
 	}
-	if !g.TryToggleSquare(position, 1+squareToggleCooldown) {
+	if !g.toggleSquare(position, 1+squareToggleCooldown) {
 		t.Fatal("toggle after cooldown was ignored")
 	}
 	if len(g.squares) != 0 {
